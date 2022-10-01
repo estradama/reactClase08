a3707d055ee9d90ed23cf4b88f4f5f9e6bbde37e
Matias Estrada <matiasunju1@gmail.com>
2022-10-01 10:50:40 -0300

    primer commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+import {Routes, Route} from 'react-router-dom'
+import Home from './routes/Home/Home';
+import PaletteDisplay from './routes/Palette/PaletteDisplay';
+import { useContext, useEffect } from 'react';
+import { getColorPalettes } from './service';
+import { ColorPalettesContext} from './context/ColorPalettesContext'
+import Navigation from './routes/Navigation/Navigation';
+import Login from './routes/Login/Login';
+import PaletteCreation from './routes/Palette/PaletteCreation';
+
+function App() {
+
+  const { setColorPalettes } = useContext(ColorPalettesContext)
+
+  useEffect(()=> {
+    getColorPalettes()
+    .then((data) => {
+      setColorPalettes(data);
+    })
+    .catch((err) => console.log(err));
+  }, [])
+
+  return (
+      <div className='App'>
+          <Routes>
+            <Route path='/' element={<Navigation/>}>
+              <Route index element={<Home/>}/>
+              <Route path='login' element={<Login/>}/>
+              <Route path='palette/:id' element={<PaletteDisplay/>}/>
+              <Route path='palette/create' element={<PaletteCreation/>}/>
+            </Route>
+          </Routes>
+      </div>
+  );
+}
+
+export default App;
